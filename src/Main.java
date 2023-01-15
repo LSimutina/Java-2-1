5113dace7fe6930448d0e9283a9ea178314a280c
Lidiya <simutina.1994@mail.ru>
2023-01-15 13:08:37 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount=5000;
+        int oneMili=20;
+        int bonus=amount/oneMili;
+
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
